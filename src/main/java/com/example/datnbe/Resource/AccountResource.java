af12f563d4869926bdf216527a321966e8590171
LAPTOP-DEH7JUPD\Pham Hieu <HieuPT73@fpt.com>
2025-02-06 17:28:24 +0700

    Đăng nhập/Đăng kí

---
+++ src/main/java/com/example/datnbe/Resource/AccountResource.java
@@ -0,0 +1,39 @@
+package com.example.datnbe.Resource;
+
+import com.example.datnbe.Entity.Request.AccountRequest;
+import com.example.datnbe.Service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AccountResource {
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AccountRequest accountRequest) {
+        boolean isAuthenticated = accountService.authenticateAccount(accountRequest.getUsername(), accountRequest.getPassword());
+
+        if (isAuthenticated) {
+            return ResponseEntity.ok("Đăng nhập thành công");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Tài khoản hoặc mật khẩu sai");
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody AccountRequest accountRequest) throws Exception {
+        boolean isRegistered = accountService.registerAccount(accountRequest.getUsername(), accountRequest.getPassword());
+
+        if (isRegistered) {
+            return ResponseEntity.ok(Map.of("message", "Đăng ký thành công!"));
+        } else {
+            return ResponseEntity.badRequest().body(Map.of("message", "Tài khoản đã tồn tại!"));
+        }
+    }
+}
